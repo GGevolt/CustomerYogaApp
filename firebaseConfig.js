c1080e590123a4de61a2fa4c0143ffc234e97751
Nguyễn Văn Bảo Duy <117926234+GGevolt@users.noreply.github.com>
2025-01-09 19:10:54 +0700

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { database };
